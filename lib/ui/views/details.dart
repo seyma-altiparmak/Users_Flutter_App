4c09207ba550882aeadc768173df27ec261414f0
Şeyma Altıparmak <102421136+seyma-altiparmak@users.noreply.github.com>
2024-04-11 15:16:23 +0300

    D1 in userapp done.

--- lib/ui/views/details.dart
+++ lib/ui/views/details.dart
@@ -1,18 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:users_flutterapp/data/entity/users.dart';
 
 class Details extends StatefulWidget {
-  const Details({super.key});
+  Users user;
+
+  Details({required this.user});
 
   @override
   State<Details> createState() => _DetailsState();
 }
 
 class _DetailsState extends State<Details> {
+  var tf_usersName = TextEditingController();
+  var tf_userPhone = TextEditingController();
+
+  Future<void> UpdateUser(int id,String name, String telephone) async{
+    print("User updated : ${id}, ${name}, ${telephone}");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    var user = widget.user;
+    tf_usersName.text = user.user_name;
+    tf_userPhone.text = user.user_phone;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("DETAILS"),),
-      body: const Center(),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.only(left: 50, right: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextField(controller: tf_usersName,
+                decoration: const InputDecoration(hintText: "Username"),),
+              TextField(controller: tf_userPhone,
+                decoration: const InputDecoration(hintText: "Phone Number"),),
+              ElevatedButton(onPressed: (){
+                UpdateUser(widget.user.user_id,tf_usersName.text,tf_userPhone.text);
+              }, child: const Text("UPDATE"),),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/ui/views/homepage.dart
+++ lib/ui/views/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:users_flutterapp/data/entity/users.dart';
+import 'package:users_flutterapp/ui/views/details.dart';
 import 'package:users_flutterapp/ui/views/register_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,17 +11,133 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isInSearch = false;
+
+  Future<void> search(String searchWord) async {
+    print("Searched : $searchWord");
+  }
+
+  Future<void> delete(int id) async {
+    print("delete : $id");
+  }
+
+  Future<List<Users>> userLoad() async {
+    var userList = <Users>[];
+    var k1 = Users(user_id: 1, user_phone: "532654852", user_name: "Aysel");
+    var k2 = Users(user_id: 2, user_phone: "524528526", user_name: "Hamide");
+    var k3 = Users(user_id: 3, user_phone: "2568596", user_name: "Saka Su");
+    userList.add(k1);
+    userList.add(k2);
+    userList.add(k3);
+
+    return userList;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("USERS"),),
-      body: const Center(),
+      appBar: AppBar(
+        title: isInSearch
+            ? TextField(
+                decoration: const InputDecoration(hintText: "Search"),
+                onChanged: (searchResult) {
+                  search(searchResult);
+                },
+              )
+            : const Text("USERS"),
+        actions: [
+          isInSearch
+              ? IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isInSearch = false;
+                    });
+                  },
+                  icon: const Icon(Icons.clear))
+              : IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isInSearch = true;
+                    });
+                  },
+                  icon: const Icon(Icons.search)),
+        ],
+      ),
+      body: FutureBuilder<List<Users>>(
+        future: userLoad(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var userList = snapshot.data;
+            return ListView.builder(
+                itemCount: userList!.length,
+                itemBuilder: (context, i) {
+                  var k = userList[i];
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Details(user: k)))
+                          .then((value) => print("NOW : HomePage"));
+                    },
+                    child: Card(
+                      child: SizedBox(
+                        height: 100,
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    k.user_name,
+                                    style: const TextStyle(fontSize: 20),
+                                  ),
+                                  Text(k.user_phone),
+                                ],
+                              ),
+                            ),
+                            const Spacer(),
+                            IconButton(
+                              onPressed: () {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                      content: Text(
+                                    "${k.user_name} 'll delete u want continue?",
+                                  ),
+                                    action: SnackBarAction(
+                                      label: "Continue",
+                                      onPressed: (){
+                                        delete((k.user_id));
+                                      },
+                                    ),
+                                  ),
+                                );
+                              },
+                              icon: Icon(Icons.delete_forever),
+                              color: Colors.black38,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                });
+          } else {
+            return const Center();
+          }
+        },
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           Navigator.push(context,
-              MaterialPageRoute(builder: (context) =>
-              const RegisterPage())).then((value) =>
-              print("NOW : Homepage"),);
+                  MaterialPageRoute(builder: (context) => const RegisterPage()))
+              .then(
+            (value) => print("NOW : Homepage"),
+          );
         },
         child: const Icon(Icons.add),
       ),
